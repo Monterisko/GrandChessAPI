d316f5826608d2b5d1ca899319659f64b6e2f520
Bartosz Siedlecki <65456083+Monterisko@users.noreply.github.com>
2025-03-25 15:30:58 +0100

    create websockets

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-from fastapi import FastAPI
+import json
+
+from fastapi import FastAPI, WebSocket
+from starlette.websockets import WebSocketDisconnect
 
 app = FastAPI()
 
+active_connections = {}
+
+@app.websocket("/ws/{game_id}")
+async def websocket_endpoint(websocket: WebSocket, game_id: str):
+    await websocket.accept()
+    if game_id not in active_connections:
+        active_connections[game_id] = []
+    active_connections[game_id].append(websocket)
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+            message = json.loads(data)
+
+            if message["type"] == "move":
+                for connection in active_connections[game_id]:
+                    await connection.send(json.dumps(message))
+    except WebSocketDisconnect:
+        active_connections[game_id].remove(websocket)
+        if not active_connections[game_id]:
+            del active_connections[game_id]
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    return {"message": "Chess websocket is running"}
